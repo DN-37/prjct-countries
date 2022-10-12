81f4b5ee52a65bb69418858599c8886eee2ecf70
Danila Nefedov <dn375@mail.ru>
2022-10-12 20:02:30 +0400

    added routes for pages NotFound, Details and HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import { Controls } from './components/Controls';
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+
 import { Header } from './components/Header';
 import { Main } from './components/Main'
 
+import { Details } from './pages/Details';
+import { HomePage } from './pages/HomePage';
+import { NotFound } from './pages/NotFound';
+
 function App() {
+  const [countries, setCountries] = useState([]);
+
   return (
     <>
       <Header />
       <Main>
-        <Controls />
+        <Routes>
+          <Route exact path='/' element={<HomePage countries={countries} setCountries={setCountries}/>} />
+          <Route path='/country/:name' element={Details} />
+          <Route element={NotFound} />
+        </Routes>
       </Main>
     </>
   );
